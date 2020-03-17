86b332045d0e8fb5c554a3eb3df98bc9d6f3f3c9
nsahakyan-vineti <narek.sahakyan@vineti.com>
2020-03-17 23:06:24 +0400

    ipnyb scrapper

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
-python3 runner.py 8800 9000
-python3 runner.py 9000 8800
+python3 runner.py 9054 9200
+python3 runner.py 9200 9400
+python3 runner.py 9400 9600
+python3 runner.py 9600 9800
+python3 runner.py 9800 10000
